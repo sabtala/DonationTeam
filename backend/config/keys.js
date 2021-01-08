24848321c3faf7b54155c50f4ddb2516ce84f26d
Michel Fopa <michelfopa@Michels-MacBook-Pro.local>
2021-01-07 20:24:21 -0600

    Established to connection to MongoBD with Mongoose

---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://fopa:fopa@cluster0.ohpdc.mongodb.net/donor"
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connection to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB is connected'))
+    .catch(err => console.log(err));
+
 
 app.get('/', (req, res) => res.send('helloooggg'));
 
