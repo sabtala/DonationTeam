102168ed76833e5b049b437eddfab22edf8da509
Michel Fopa <michelfopa@Michels-MacBook-Pro.local>
2021-01-08 13:49:57 -0600

    Send token when user login

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://fopa:fopa@cluster0.ohpdc.mongodb.net/donor"
+    mongoURI: "mongodb+srv://fopa:fopa@cluster0.ohpdc.mongodb.net/donor",
+    secreteOrKey: 'secret'
 }
